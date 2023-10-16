44dc79f12a43b4d02b404ddb78af99465010d8ab
João Victor Macedo <gmvictorr@outlook.com>
2023-10-16 16:22:16 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://www.alura.com.br/artigos/assets/como-comecar-com-angular/como-comecar-com-angular.png",
+        "title": "Nova versão do Angular",
+        "description": "Framework mais usado entre os devs ganhará nova versão com maior número de features para promover acessibilidade"
+    },
+    {
+        "id": "2",
+        "photoCover": "https://terminalroot.com.br/assets/img/dev/linguagens-de-programacao-2.jpg",
+        "title": "Qual a linguagem mais rapida?",
+        "description": "Programadores discutem qual a linguagem mais rapida atuante no mercado"
+    },
+    {
+        "id": "3",
+        "photoCover": "https://educandocerto.com.br/wp-content/uploads/2022/08/c.jpg",
+        "title": "C++: te lembrando que o computador é um dispositivo físico finito",
+        "description": "Como a necessidade de alocar memória faz o desenvolvedor não perder a noção da finitude da máquina."
+    },
+    {
+        "id": "4",
+        "photoCover": "https://spectrum.ieee.org/media-library/image.jpg?id=33364099",
+        "title": "Python: inglês estrutural? prós e contras de uma linguagem interpretada.",
+        "description": "Linguagens interpretadas, códigos enxutos e maior facilidade. Porém, qual o custo destes benefícios?"
+    }
+]
